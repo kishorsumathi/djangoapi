0ccf5643157f77d140ad9c2328faf30087185abf
kishor <kishorbrindha18@gmail.com>
2022-07-21 18:55:05 +0530

    fileresponse addded

--- restapi/drinks/urls.py
+++ restapi/drinks/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from drinks.views import drink_list
+from drinks.views import drink_list,send_file
 
 urlpatterns = [
     path('', drink_list,name="drinks"),
+    path("drinks/<path:filename>",send_file)
 ]
--- restapi/drinks/views.py
+++ restapi/drinks/views.py
@@ -1,15 +1,36 @@
+from turtle import st
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import Drinkserializer
+from  rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import FileResponse
+from rest_framework import status
 
 # Create your views here.
-
+@api_view(["GET","POST"])
 def drink_list(request):
     ##get all the drinks
     #serialize them
     #return json
-    drinks=Drink.objects.all()
-    serializer=Drinkserializer(drinks,many=True)
-    return JsonResponse(serializer.data,safe=False)
+    if request.method=="GET":
+        drinks=Drink.objects.all()
+        serializer=Drinkserializer(drinks,many=True)
+        return JsonResponse(serializer.data,safe=False)
+    if request.method=="POST":
+        serializer=Drinkserializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+@api_view(["GET"])
+def send_file(request,filename):
+
+    img = open(filename, 'rb')
+
+    response = FileResponse(img)
+
+    return response
+        
+
     
